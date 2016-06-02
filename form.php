ac9e5349cfdc79ca6648493f154a5e18236764fc
Dan <daniel@incubateinstitute.org>
2016-06-01 21:30:42 -0400

    V4

--- form.php
+++ form.php
@@ -1,28 +1,41 @@
 <?php
-if(filter_input(INPUT_POST, 'submit'))
-{
-	
-	$name = trim(filter_input(INPUT_POST, 'contactName')," ");
-	$email = "From:" . trim(filter_input(INPUT_POST, 'contactEmail')," ");
-	$message = trim(filter_input(INPUT_POST, 'contactMessage')," ");
-	$captcha = trim(filter_input(INPUT_POST, 'contactCaptcha')," ");
-	$to = 'danlee746@hotmail.ca';
-	if(!empty($name) AND !empty($email) AND !empty($message) AND $captcha == 2)
-	{
-		if(filter_var($email, FILTER_VALIDATE_EMAIL))
-		{
-			mail($to,$name,$message,$email);
-                        header("Location:index.html");
-		}
-		else {
-			die("Incorrect E-mail Format!");
-		}
-		
-	}
-	else 
-	{
-		die("A required field is empty");
-	}
-}
 
+ $url = 'https://api.sendgrid.com/';
+ $user = 'azure_10f17c72120cc0aebfd1cc8e7fdb3271@azure.com';
+ $pass = 'Your Password'; 
+ $email = $_POST['contactEmail'];
+ $message = $_POST['contactMessage'];
+ $name = $_POST['contactName'];
+ $params = array(
+      'api_user' => $user,
+      'api_key' => $pass,
+      
+      'to' => 'danlee746@hotmail.ca',
+      'subject' => $name,
+      'html' => 'testing body',
+      'text' => $message,
+      'from' => $email,
+   );
+
+ $request = $url.'api/mail.send.json';
+
+ // Generate curl request
+ $session = curl_init($request);
+
+ // Tell curl to use HTTP POST
+ curl_setopt ($session, CURLOPT_POST, true);
+
+ // Tell curl that this is the body of the POST
+ curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+ // Tell curl not to return headers, but do return the response
+ curl_setopt($session, CURLOPT_HEADER, false);
+ curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+ // obtain response
+ $response = curl_exec($session);
+ curl_close($session);
+
+ // print everything out
+ print_r($response);
 ?>
